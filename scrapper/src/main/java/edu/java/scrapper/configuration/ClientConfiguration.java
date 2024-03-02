6d7553339c1881de650b4d2010f212d969d1d6fa
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2024-03-02 15:47:22 +0300

    Scrapper: Add client configurations

--- scrapper/src/main/java/edu/java/scrapper/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ClientConfiguration.java
@@ -1,14 +1,19 @@
 package edu.java.scrapper.configuration;
 
 import edu.java.scrapper.client.GithubClient;
+import edu.java.scrapper.client.ScrapperClient;
 import edu.java.scrapper.client.StackOverflowClient;
+import edu.java.scrapper.dto.response.ApiErrorResponse;
+import edu.java.scrapper.exception.ApiException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
@@ -27,16 +32,30 @@ public class ClientConfiguration {
     @Bean
     public StackOverflowClient stackOverflowClient(@Value("https://api.stackexchange.com") String baseUrl) {
         HttpClient httpClient = HttpClient.create()
-            .baseUrl(baseUrl)
-            .compress(true);
+                .baseUrl(baseUrl)
+                .compress(true);
 
         WebClient webClient = WebClient.builder()
-            .clientConnector(new ReactorClientHttpConnector(httpClient))
-            .build();
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
 
         WebClientAdapter adapter = WebClientAdapter.create(webClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
 
         return factory.createClient(StackOverflowClient.class);
+    }
+
+    public ScrapperClient scrapperClient(@Value("http://localhost:8090") String baseUrl) {
+        WebClient webClient = WebClient.builder()
+            .baseUrl(baseUrl)
+            .defaultStatusHandler(
+                HttpStatusCode::is4xxClientError,
+                response -> response.bodyToMono(ApiErrorResponse.class)
+                    .flatMap(apiErrorResponse -> Mono.error(new ApiException(apiErrorResponse)))
+            )
+            .build();
+        WebClientAdapter adapter = WebClientAdapter.create(webClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+        return factory.createClient(ScrapperClient.class);
     }
 }
