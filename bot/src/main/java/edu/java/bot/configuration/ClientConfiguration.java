4218fb3feff58d50bbe20be29f0e34ed637836b1
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2024-03-02 13:08:56 +0300

    Bot: Add error handling to client

--- bot/src/main/java/edu/java/bot/configuration/ClientConfiguration.java
+++ bot/src/main/java/edu/java/bot/configuration/ClientConfiguration.java
@@ -1,12 +1,16 @@
 package edu.java.bot.configuration;
 
 import edu.java.bot.client.BotClient;
+import edu.java.bot.dto.ApiErrorResponse;
+import edu.java.bot.exception.ApiException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class ClientConfiguration {
@@ -14,6 +18,11 @@ public class ClientConfiguration {
     public BotClient botClient(@Value("http://localhost:8080") String baseUrl) {
         WebClient webClient = WebClient.builder()
             .baseUrl(baseUrl)
+            .defaultStatusHandler(
+                HttpStatusCode::is4xxClientError,
+                response -> response.bodyToMono(ApiErrorResponse.class)
+                    .flatMap(apiErrorResponse -> Mono.error(new ApiException(apiErrorResponse)))
+            )
             .build();
         WebClientAdapter adapter = WebClientAdapter.create(webClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
