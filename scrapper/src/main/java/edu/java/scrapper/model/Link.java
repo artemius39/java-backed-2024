240ce2d872dd9e442b254cb71ff58f27659494ad
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2024-03-16 15:37:19 +0300

    Add constructors to Link

--- scrapper/src/main/java/edu/java/scrapper/model/Link.java
+++ scrapper/src/main/java/edu/java/scrapper/model/Link.java
@@ -11,4 +11,13 @@ public class Link {
     private Long id;
     private URI url;
     private OffsetDateTime lastUpdated;
+
+    public Link(URI url) {
+        this.url = url;
+    }
+
+    public Link(Long id, URI url) {
+        this.id = id;
+        this.url = url;
+    }
 }
