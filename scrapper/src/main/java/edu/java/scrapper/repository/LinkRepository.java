5128b8791e100f9629659a851389dccffb3ba09a
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2024-03-16 15:37:39 +0300

    Add findByUserId to LinkRepository

--- scrapper/src/main/java/edu/java/scrapper/repository/LinkRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/repository/LinkRepository.java
@@ -2,9 +2,12 @@ package edu.java.scrapper.repository;
 
 import edu.java.scrapper.model.Link;
 import edu.java.scrapper.model.User;
+import java.util.Collection;
 
 public interface LinkRepository {
     Link add(Link link, User user);
 
-    void remove(Link link, User user);
+    Link remove(Link link, User user);
+
+    Collection<Link> findByUserId(Long userId);
 }
