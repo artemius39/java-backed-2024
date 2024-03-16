0c66499e746bcfc9d0cb3ddec6bc8aec71d2cd5f
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2024-03-16 13:26:21 +0300

    Remove findAll operations

--- scrapper/src/main/java/edu/java/scrapper/repository/LinkRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/repository/LinkRepository.java
@@ -1,12 +1,9 @@
 package edu.java.scrapper.repository;
 
 import edu.java.scrapper.model.Link;
-import java.util.List;
 
 public interface LinkRepository {
     Link add(Link link);
 
     void remove(Link link);
-
-    List<Link> findAll();
 }
